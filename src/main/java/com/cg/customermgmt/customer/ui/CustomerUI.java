1d2f9c9473a96d007c3fd64813563397caf2c33f
tanisha-garg <tanishagarg804@gmail.com>
2021-03-15 22:15:10 +0530

    Completed Task 1

--- src/main/java/com/cg/customermgmt/customer/ui/CustomerUI.java
+++ src/main/java/com/cg/customermgmt/customer/ui/CustomerUI.java
@@ -22,63 +22,69 @@ public class CustomerUI {
 	
 	public void start() {
 		
-		Customer tanisha = service.createCustomer("Tanisha");
-		display(tanisha);
-		
+		Customer tanisha = service.createCustomer("Tanisha");		
 		Customer pallavi = service.createCustomer("Pallavi");
-		display(pallavi);
+		
+		Item chocolate = itemService.create(100.0, "Chocolate");		
+		Item rice = itemService.create(200.0, "Rice");
+		Item maggi = itemService.create(100, "Maggi");
 		
 		System.out.println("Customer with id 4");
 		Customer findCustomer = service.findById(4L);
 		display(findCustomer);
 		
+		System.out.println("*************************");
 		System.out.println("After adding amount in Tanisha's Account");
 		Long tanishaId = tanisha.getId();
 		Customer tanishaAmount = service.addAmount(tanishaId, 1000.0);
 		display(tanishaAmount);
 		
-		Item chocolate = itemService.create(100.0, "Chocolate");
-		displayItem(chocolate);
-		
-		Item rice = itemService.create(200.0, "Rice");
-		displayItem(rice);
-		
+		System.out.println("*************************");
 		System.out.println("Finding an item with id");
 		String itemId = chocolate.getItem();
 		Item findItem = itemService.findById(itemId);
 		displayItem(findItem);
 		
+		System.out.println("*************************");
 		System.out.println("Buy an item");
 		Item buyChocolate = itemService.buyItem(itemId, tanishaId);
 		tanisha = service.findById(tanishaId);
 		displayItem(buyChocolate);
 		display(tanisha);
 		
+		System.out.println("*************************");
 		Item buyRice = itemService.buyItem(rice.getItem(), pallavi.getId());
 		pallavi = service.findById(pallavi.getId());
 		displayItem(buyRice);
 		display(pallavi);
 		
+		System.out.println("*************************");
+		Item buyMaggi = itemService.buyItem(maggi.getItem(), tanishaId);
+		tanisha = service.findById(tanishaId);
+		displayItem(buyMaggi);
+		display(tanisha);
+		
 	}
 	
 	void display(Customer customer) {
 		Account account = customer.getAccount();
-		System.out.println("Customer "+customer.getId()+" "+customer.getName()+" "+account.getAccountId()+" "
-		+account.getBalance()+" "+account.getCreated());
+		System.out.println("Customer "+customer.getId()+":\nName: "+customer.getName()
+		+"\nAccountId: "+account.getAccountId()+"\nBalance: "
+		+account.getBalance()+"\nCreated: "+account.getCreated());
 		Set<Item> itemSet = customer.getBoughtItems();
 		for(Item item:itemSet) {
-			System.out.println(item.getItem()+" "+item.getDescription());	
+			System.out.println("Item Details: "+item.getItem()+" "+item.getDescription());	
 		}
 	}
 	
 	void displayItem(Item item) {
 		if(item.getBoughtBy() == null) {
-			System.out.println("Item "+item.getItem()+" "+item.getDescription()+" "+item.getPrice()+" "
-					+item.getAddedDate());
+			System.out.println("Item "+item.getItem()+":\nName: "+item.getDescription()+"\nPrice: "
+		+item.getPrice()+"\nAdded on: "+item.getAddedDate());
 		}
 		else {
-			System.out.println("Item "+item.getItem()+" "+item.getDescription()+" "+item.getPrice()+" "
-					+item.getAddedDate()+" "+item.getBoughtBy().getName());
+			System.out.println("Item "+item.getItem()+":\nName: "+item.getDescription()+"\nPrice "
+		+item.getPrice()+"\nAdded on: "+item.getAddedDate()+"\nBought by: "+item.getBoughtBy().getName());
 		}
 		
 	}
